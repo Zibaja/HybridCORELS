439b34be88c7324587887567b1a2228a3c5dfea0
Ziba Jabbar Zare <ziba@Zibas-MacBook-Air.local>
2026-04-08 17:00:11 -0400

    Save current change

---
+++ paper/Run_Boots_CRL_batch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=hybridCOREL_bootstrap
+#SBATCH --array=0-99
+#SBATCH --time=08:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --output=logs/%A_%a.out
+#SBATCH --error=logs/%A_%a.err
+
+# Load Python module 
+module load python/3.10
+
+
+
+# Create folders if not exist
+mkdir -p logs
+mkdir -p bootstrap_results
+
+# Run script
+python Run_Boots.py \
+    --dataset compas \
+    --model CRL \
+    --seed 0 \
+    --round_min 0 \
+    --round_max 9 \
+    --local_id $SLURM_ARRAY_TASK_ID
